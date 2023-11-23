6becf406567e15473f75c8c6492cdf84c2f09019
Jiyjo Jose <jiyjo@Jerrishs-MacBook-Main.local>
2023-11-23 13:26:09 +0530

    Refactoring

--- src/test/java/com/edstem/habitReminder/HabitReminderApplicationTests.java
+++ src/test/java/com/edstem/habitReminder/HabitReminderApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HabitTrackerApplicationTests {
+class HabitReminderApplicationTests {
 
     @Test
     void contextLoads() {}
